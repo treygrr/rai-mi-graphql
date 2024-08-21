d1edbd155cc6093adee3fb239148940861355701
Trey <trey.rogers@rexelusa.com>
2024-08-21 17:26:34 -0400

    Oh god I forgot

--- src/schema.ts
+++ src/schema.ts
@@ -5,37 +5,85 @@ type Product = {
   id: string,
   title: string,
   stock: number,
-  qty: number,
+  price: number
+}
+
+type Cart = {
+  id: string,
+  lines: CartLine[]
+}
+type CartLine = {
+  id: string,
+  product: Product,
+  qty: number
+}
+type QueryCart = Cart & {
+  lineCount: number,
+  total: number
 }
 
 type UpdateProductInput = Product
+type AddProductToCartInput = {
+  cartId: string,
+  productId: string,
+  qty: number
+}
 
 const products: Product[] = [
   {
-    id: randomUUID(),
+    id: "8bceb990-9335-403b-9056-496f275cb8f4",
     title: 'BR120',
     stock: 48923,
-    qty: 0
+    price: 5.17
   },
   {
     id: randomUUID(),
     title: 'Battery',
     stock: 4100,
-    qty: 0
+    price: 10.50
   },
   {
     id: randomUUID(),
     title: 'Oil Funnel',
     stock: 1410,
-    qty: 0
+    price: 2.18
   },
   {
     id: randomUUID(),
     title: 'Wrench',
     stock: 2001,
-    qty: 0
+    price: 5.43
   }
 ]
+
+const carts: Cart[] = [
+  {
+    id: '5a797e70-cdba-4738-b7a5-ca6a63a2ddc0',
+    lines: [
+      {
+        id: randomUUID(),
+        product: products[0],
+        qty: 5,
+      }
+    ],
+  }
+]
+
+function calculateCart(cart: Cart): QueryCart {
+  const lineCount = cart.lines.length;
+  const total = cart.lines.reduce((sum, line) => sum + (line.product.price * line.qty), 0);
+  return {
+    ...cart,
+    lineCount,
+    total
+  };
+}
+
+function getCarts (): QueryCart[] {
+  return carts.map(cart => {
+    return calculateCart(cart)
+  })
+}
 
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
@@ -43,24 +91,44 @@ export const schema = createSchema({
       id: ID!,
       title: String!,
       stock: Int!,
+      price: Float!
+    }
+    type Cart {
+      id: ID!,
+      lines: [CartLine]!,
+      lineCount: Int!,
+      total: Float!
+    }
+    type CartLine {
+      id: ID!,
+      product: Product!,
       qty: Int!
+      total: Int!,
     }
     input UpdateProductInput {
       id: ID!,
       title: String!,
       stock: Int!,
+      price: Float!
+    }
+    input AddProductToCartInput {
+      cartId: ID!,
+      productId: ID!,
       qty: Int!
     }
     type Query {
-      products: [Product]!
+      products: [Product!]!,
+      carts: [Cart!]!
     }
     type Mutation {
-      updateProduct(input: UpdateProductInput): Product!
+      updateProduct(input: UpdateProductInput!): Product!,
+      addProductToCart(input: AddProductToCartInput!): [Cart]!
     }
   `,
   resolvers: {
     Query: {
-      products: () => products
+      products: () => products,
+      carts: () => getCarts()
     },
     Mutation: {
       updateProduct: (parent: unknown, args: { input: UpdateProductInput }) => {
@@ -71,14 +139,35 @@ export const schema = createSchema({
 
         if (productRecord !== -1) {
           products[productRecord] = args.input
-          console.log('set products', products[productRecord])
-          console.log(products)
         } else {
           products.push(args.input)
-          console.log('pushed product', args.input)
           return args.input
         }
         return products[productRecord]
+      },
+      addProductToCart: (parent: unknown, args: { input: AddProductToCartInput }) => {
+        const { cartId, productId, qty } = args.input
+        const cart = carts.find(cart => cart.id === cartId)
+        const product = products.find(product => product.id === productId)
+
+        if (!cart || !product) {
+          throw new Error('Cart or Product not found')
+        }
+
+
+        const isInCart = cart.lines.find(line => line.product.id === product.id)
+        if (isInCart) {
+          isInCart.qty = isInCart.qty + qty
+          return getCarts()
+        }
+        const cartLine: CartLine = {
+          id: randomUUID(),
+          product,
+          qty
+        }
+
+        cart.lines.push(cartLine)
+        return getCarts()
       }
     }
   }
